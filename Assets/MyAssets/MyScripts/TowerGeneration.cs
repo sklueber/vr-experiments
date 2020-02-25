cd57e6de405972fc30acbb3edbedc391e61ae36a
Simon <simon.klueber@t-online.de>
2020-02-26 00:52:41 +0100

    completely parameterized working tower generation (!!!), blocks are SteamVR throwables

--- Assets/MyAssets/MyScripts/TowerGeneration.cs
+++ Assets/MyAssets/MyScripts/TowerGeneration.cs
@@ -1,29 +1,50 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerGeneration : MonoBehaviour
 {
     public GameObject blockPrefab;
-    
+    public float tableHight;
+    public float tableZ;
+    public float tableX;
+    public int towerHight;
+    public float dropHeight;
+    float blockLength;
+    float blockHight;
+    float blockWidth;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(waiter());
+        StartCoroutine(BuildTower());
+        Vector3 blockScale = blockPrefab.transform.localScale;
+        blockLength = blockScale.z;
+        blockHight = blockScale.y;
+        blockWidth = blockScale.x;
     }
 
-    IEnumerator waiter()
+    IEnumerator BuildTower()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < towerHight; i += 2) //rows
         {
-            Instantiate(blockPrefab, new Vector3(0, 0.53F + i * 0.015F, 1.58F), Quaternion.identity);
-            yield return new WaitForSeconds(1);
+            for (int j = 0; j < 3; j++) //row of blocks facing in z-axis
+            {
+                Instantiate(blockPrefab, new Vector3(tableX + j * blockWidth, tableHight + dropHeight + i * blockHight, tableZ), Quaternion.identity); //TODO parameterize starting position of tower
+
+                yield return new WaitForSeconds(1);
+            }
+            for (int k = 0; k < 3; k++) //row of blocks facing in x-axis
+            {
+                Instantiate(blockPrefab, new Vector3(tableX + blockWidth, tableHight + dropHeight + i * blockHight + blockHight, tableZ + (k - 1) * blockWidth), Quaternion.AngleAxis(90, Vector3.up)); //for z: middle block has same z as the "z-axis" blocks
+
+                yield return new WaitForSeconds(1);
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
