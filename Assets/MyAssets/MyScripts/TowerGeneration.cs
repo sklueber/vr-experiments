023a66c5b32df55d0079aa710d3bdc4c28e90fe4
Simon <simon.klueber@t-online.de>
2020-02-25 18:03:25 +0100

    table and blocks including materials, edited "Solver Iteration Count" to 30 for stacking

---
+++ Assets/MyAssets/MyScripts/TowerGeneration.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerGeneration : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(blockPrefab, new Vector3(0, 0.53F + i * 0.015F, 1.58F), Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
