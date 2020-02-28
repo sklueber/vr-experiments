36f0592b8b7aa15f046bd5bfc6ad54909f2fd2a8
Simon <simon.klueber@t-online.de>
2020-02-28 22:48:03 +0100

    reset button despawns all blocks and starts new TowerGeneration

---
+++ Assets/MyAssets/MyScripts/MyButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+
+public class MyButtonScript : MonoBehaviour
+{
+    //public GameObject table;
+    TowerGeneration genScript;
+    public GameObject blockPrefab;
+
+    public void Start()
+    {
+        genScript = GameObject.FindObjectOfType(typeof(TowerGeneration)) as TowerGeneration;
+
+    }
+    public void OnButtonDown()
+    {
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Block"))
+        {
+            Destroy(obj);
+        }
+        genScript.Invoke("Start", 0);
+    }
+
+    public void OnButtonUp()
+    {
+
+    }
+}
