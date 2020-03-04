3d79dbcfef373ea4f5b4967bfb14ceec362bdef2
Simon <simon.klueber@t-online.de>
2020-03-05 00:41:29 +0100

    implemented bare-bones, naive scoring system

---
+++ Assets/MyAssets/MyScripts/ScoringSystem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringSystem : MonoBehaviour
+{
+    public GameObject scoreLabel; //TODO specify
+    int score;
+    int highscore;
+    bool onNewHighscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        highscore = 0; //could be replaced by savefile but that's a bit too much right now
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void incremenetScore()
+    {
+        score++;
+        if (score > highscore)
+        {
+            highscore = score;
+            if(!onNewHighscore)
+            {
+                achievedNewHighscore();
+            }
+        }
+        updateLabel();
+    }
+
+    private void updateLabel()
+    {
+        Debug.Log($"Score: {score}, Highscore: {highscore}");
+    }
+
+    private void achievedNewHighscore()
+    {
+        Debug.Log("Congratulations");
+    }
+}
